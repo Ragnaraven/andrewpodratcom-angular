8165e480db2b232d5527493fc21d4b661cc927e0
Subliminally Sublime <kingofhalolocally@live.com>
2020-11-16 13:16:11 -0500

    Before rework navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { AppsComponent } from './apps/apps.component';
+import { ArtComponent } from './art/art.component';
+
 import { ProjectComponent } from './project/project.component';
 import { ImageTextComponent } from './image-text/image-text.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  
+  { path: 'games', component: GamesComponent },
+  { path: 'apps', component: AppsComponent },
+  { path: 'art', component: ArtComponent },
+
   { path: 'project', component: ProjectComponent },
-  { path: 'imageText', component: ImageTextComponent }
+  { path: 'imageText', component: ImageTextComponent },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }
 ];
 
 @NgModule({
